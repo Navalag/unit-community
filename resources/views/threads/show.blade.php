7492cbb08041a53a1af99de6ad0d01d39e67acb5
Navalag <agalavan@student.unit.ua>
2019-05-24 21:03:00 +0300

    reply form added

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,6 +5,9 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card mb-3">
+                    <a href="#">
+                        {{ $thread->creator->name }}
+                    </a> posted:
                     <div class="card-header">{{ $thread->title }}</div>
 
                     <div class="card-body">
@@ -17,18 +20,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 @foreach($thread->replies as $reply)
-                    <div class="card mb-3">
-                        <div class="card-header">
-                            <a href="#">
-                                {{ $reply->owner->name }}
-                            </a>
-                             said {{ $reply->created_at->diffForHumans() }}
-                        </div>
-
-                        <div class="card-body">
-                            {{ $reply->body }}
-                        </div>
-                    </div>
+                    @include('threads.reply')
                 @endforeach
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('/', function () {
 
 Route::get('/threads', 'ThreadsController@index');
 Route::get('/threads/{thread}', 'ThreadsController@show');
+
+Route::post('/threads/{thread}/replies', 'RepliesController@store');
