e1423691028fa5871806ee4eda1e2a1c6bc7cd32
Navalag <agalavan@student.unit.ua>
2019-05-24 19:35:20 +0300

    first commit

---
+++ database/seeds/Development/ReplyTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Reply;
+use App\Thread;
+use Illuminate\Database\Seeder;
+
+class ReplyTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $threads = Thread::all();
+
+        $threads->each(function ($thread) {
+            factory(Reply::class, 10)->create(['thread_id' => $thread->id]);
+        });
+    }
+}
