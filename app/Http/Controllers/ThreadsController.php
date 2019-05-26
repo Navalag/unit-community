8b3bcc18061fc3d5c3c71cbb2340ff9831801f73
Navalag <agalavan@student.unit.ua>
2019-05-26 14:28:54 +0300

    added validation errors and old data

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Channel;
 use App\Thread;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Exception;
 
 class ThreadsController extends Controller
 {
@@ -16,7 +18,8 @@ class ThreadsController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @param  Channel  $channel
+     * @return Response
      */
     public function index(Channel $channel)
     {
@@ -32,7 +35,7 @@ class ThreadsController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
@@ -42,8 +45,10 @@ class ThreadsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  Request  $request
+     * @return Response
+     *
+     * @throws Exception
      */
     public function store(Request $request)
     {
@@ -67,8 +72,8 @@ class ThreadsController extends Controller
      * Display the specified resource.
      *
      * @param $channelId
-     * @param  \App\Thread  $thread
-     * @return \Illuminate\Http\Response
+     * @param  Thread  $thread
+     * @return Response
      */
     public function show($channelId, Thread $thread)
     {
@@ -78,8 +83,8 @@ class ThreadsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Thread  $thread
-     * @return \Illuminate\Http\Response
+     * @param  Thread  $thread
+     * @return Response
      */
     public function edit(Thread $thread)
     {
@@ -89,9 +94,9 @@ class ThreadsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Thread  $thread
-     * @return \Illuminate\Http\Response
+     * @param  Request  $request
+     * @param  Thread  $thread
+     * @return Response
      */
     public function update(Request $request, Thread $thread)
     {
@@ -101,8 +106,8 @@ class ThreadsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Thread  $thread
-     * @return \Illuminate\Http\Response
+     * @param  Thread  $thread
+     * @return Response
      */
     public function destroy(Thread $thread)
     {
