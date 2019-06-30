bb83d161f73d0e45e83a17fa3265efa9e4a941e7
Navalag <agalavan@student.unit.ua>
2019-06-30 13:25:11 +0300

    updated thread subscription process

---
+++ tests/Feature/SubscribeToThreadsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SubscribeToThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function a_user_can_subscribe_to_threads()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $this->post($thread->path() . '/subscriptions');
+
+        $thread->addReply([
+            'user_id' => auth()->id(),
+            'body' => 'Some reply here.'
+        ]);
+    }
+}
