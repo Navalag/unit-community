53570f77dcbf9ab8531dd1c3bf95076e20a716bc
Navalag <agalavan@student.unit.ua>
2019-07-22 16:20:57 +0300

    small fixes for production mode

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Faker\Factory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,9 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call([
-             ThreadTableSeeder::class,
-             ReplyTableSeeder::class,
-         ]);
+        $faker = Factory::create();
+        $users = factory(User::class, 20)->create();
+        $channels = factory(App\Channel::class, 6)->create()->toArray();
+
+        $users->each(function ($user) use ($channels, $faker) {
+            $thread = factory(App\Thread::class)->create([
+                'user_id' => $user->id,
+                'channel_id' => $faker->randomElements($channels)[0]['id'],
+            ]);
+
+            factory(App\Reply::class, 5)->create([
+                'thread_id' => $thread->id,
+                'user_id' => $user->id,
+            ]);
+        });
     }
 }
